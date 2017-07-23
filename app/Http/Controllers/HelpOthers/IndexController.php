b45e6a08e774f68883718594cc527cd9e6e233be
jiangbaiyan <987082332@qq.com>
2017-07-23 11:22:30 +0800

    2017年7月23日 修复返回字段缺失的BUG，并且取消了cookie和session

--- app/Http/Controllers/HelpOthers/IndexController.php
+++ app/Http/Controllers/HelpOthers/IndexController.php
@@ -9,37 +9,37 @@ use Illuminate\Support\Facades\Response;
 class IndexController extends Controller
 {
     public function getAllOrders(){
-        $Orders = Order::join('users','orders.applicant','=','users.phone')->select('orders.id','orders.title','orders.category','orders.state','orders.created_at','users.name','users.credit')->where('orders.state',1)->orderBy('orders.created_at','desc')->get();
+        $Orders = Order::join('users','orders.applicant','=','users.phone')->select('orders.id','orders.title','orders.category','orders.state','orders.money','orders.created_at','users.name','users.credit')->where('orders.state',1)->orderBy('orders.created_at','desc')->get();
         return Response::json(['status' => 200,'msg' => 'all orders required successfully','data' => $Orders]);
     }
 
     public function getRunOrders(){
-        $Orders = Order::join('users','orders.applicant','=','users.phone')->select('orders.id','orders.title','orders.category','orders.state','orders.created_at','users.name','users.credit')->where(['orders.state' => 1,'orders.category' => 1])->orderBy('orders.created_at','desc')->get();
+        $Orders = Order::join('users','orders.applicant','=','users.phone')->select('orders.id','orders.title','orders.category','orders.state','orders.money','orders.created_at','users.name','users.credit')->where(['orders.state' => 1,'orders.category' => 1])->orderBy('orders.created_at','desc')->get();
         return Response::json(['status' => 200,'msg' => 'run orders required successfully','data' => $Orders]);
     }
 
     public  function getAskOrders(){
-        $Orders = Order::join('users','orders.applicant','=','users.phone')->select('orders.id','orders.title','orders.category','orders.state','orders.created_at','users.name','users.credit')->where(['orders.state' => 1,'orders.category' => 2])->orderBy('orders.created_at','desc')->get();
+        $Orders = Order::join('users','orders.applicant','=','users.phone')->select('orders.id','orders.title','orders.category','orders.state','orders.money','orders.created_at','users.name','users.credit')->where(['orders.state' => 1,'orders.category' => 2])->orderBy('orders.created_at','desc')->get();
         return Response::json(['status' => 200,'msg' => 'ask orders required successfully','data' => $Orders]);
     }
 
     public function getLearnOrders(){
-        $Orders = Order::join('users','orders.applicant','=','users.phone')->select('orders.id','orders.title','orders.category','orders.state','orders.created_at','users.name','users.credit')->where(['orders.state' => 1,'orders.category' => 3])->orderBy('orders.created_at','desc')->get();
+        $Orders = Order::join('users','orders.applicant','=','users.phone')->select('orders.id','orders.title','orders.category','orders.state','orders.money','orders.created_at','users.name','users.credit')->where(['orders.state' => 1,'orders.category' => 3])->orderBy('orders.created_at','desc')->get();
         return Response::json(['status' => 200,'msg' => 'learn orders required successfully','data' => $Orders]);
     }
 
     public function getTechniqueOrders(){
-        $Orders = Order::join('users','orders.applicant','=','users.phone')->select('orders.id','orders.title','orders.category','orders.state','orders.created_at','users.name','users.credit')->where(['orders.state' => 1,'orders.category' => 4])->orderBy('orders.created_at','desc')->get();
+        $Orders = Order::join('users','orders.applicant','=','users.phone')->select('orders.id','orders.title','orders.category','orders.state','orders.money','orders.created_at','users.name','users.credit')->where(['orders.state' => 1,'orders.category' => 4])->orderBy('orders.created_at','desc')->get();
         return Response::json(['status' => 200,'msg' => 'technique orders required successfully','data' => $Orders]);
     }
 
     public function getLifeOrders(){
-        $Orders = Order::join('users','orders.applicant','=','users.phone')->select('orders.id','orders.title','orders.category','orders.state','orders.created_at','users.name','users.credit')->where(['orders.state' => 1,'orders.category' => 5])->orderBy('orders.created_at','desc')->get();
+        $Orders = Order::join('users','orders.applicant','=','users.phone')->select('orders.id','orders.title','orders.category','orders.state','orders.money','orders.created_at','users.name','users.credit')->where(['orders.state' => 1,'orders.category' => 5])->orderBy('orders.created_at','desc')->get();
         return Response::json(['status' => 200,'msg' => 'life orders required successfully','data' => $Orders]);
     }
 
     public function getOtherOrders(){
-        $Orders = Order::join('users','orders.applicant','=','users.phone')->select('orders.id','orders.title','orders.category','orders.state','orders.created_at','users.name','users.credit')->where(['orders.state' => 1,'orders.category' => 6])->orderBy('orders.created_at','desc')->get();
+        $Orders = Order::join('users','orders.applicant','=','users.phone')->select('orders.id','orders.title','orders.category','orders.state','orders.money','orders.created_at','users.name','users.credit')->where(['orders.state' => 1,'orders.category' => 6])->orderBy('orders.created_at','desc')->get();
         return Response::json(['status' => 200,'msg' => 'other orders required successfully','data' => $Orders]);
     }
 }

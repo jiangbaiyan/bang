d29fa9ccda97f3d1d8fae7688d3b2a0955858384
Toby <987082332@qq.com>
2018-07-04 14:00:42 +0800

    cert key

--- config/wx.php
+++ config/wx.php
@@ -15,7 +15,7 @@ return [
     'pay' => [
         'miniapp_id' => 'wx7e2caeca5c50a086', // appId
         'mch_id' => '1508225431',//商户id
-        'key' => 'c1f7a5af2f140e9811a1290c185faff8',//支付的key
+        'key' => 'hdu_qlc_2018_01bdf25*9',//支付的key
         'notify_url' => \App\Helper\ConstHelper::HOST . 'pay/notify',
         'cert_client' => './storage/cert/apiclient_cert.pem', // optional, 退款，红包等情况时需要用到
         'cert_key' => './storage/cert/apiclient_key.pem',// optional, 退款，红包等情况时需要用到

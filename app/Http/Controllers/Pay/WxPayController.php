c851ec446384fab58787e8c589f111134b02bcf7
Toby <987082332@qq.com>
2018-07-04 22:54:11 +0800

    还原扩展包

--- app/Http/Controllers/Pay/WxPayController.php
+++ app/Http/Controllers/Pay/WxPayController.php
@@ -11,23 +11,22 @@ namespace App\Http\Controllers\Pay;
 use App\Helper\ConstHelper;
 use App\Http\Controllers\Controller;
 use App\Model\OrderModel;
-use App\Service\WxService;
 use App\UserModel;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Validator;
 use src\ApiHelper\ApiResponse;
 use src\Exceptions\OperateFailedException;
 use src\Exceptions\ParamValidateFailedException;
+use Yansongda\Pay\Pay;
 
 class WxPayController extends Controller{
-
 
     /**
      * 统一下单
      * @param Request $request
      * @return string
      * @throws ParamValidateFailedException
-     * @throws \EasyWeChat\Kernel\Exceptions\InvalidConfigException
      * @throws \src\Exceptions\ResourceNotFoundException
      * @throws \src\Exceptions\UnAuthorizedException
      */
@@ -39,28 +38,26 @@ class WxPayController extends Controller{
         }
         $order = OrderModel::getOrderById($req['id']);
         $user = UserModel::getCurUser();
-        $app = WxService::getEasyApp();
-        $result = $app->order->unify([
-            'body' => $order->title,
+        $params = [
             'out_trade_no' => time(),
-            'total_fee' => ($order->price) * 100,
-            'trade_type' => 'JSAPI',
+            'total_fee' => ($order->price) * 100, // **单位：分**
+            'body' => $order->title,
             'openid' => $user->openid,
-        ]);
-        return ApiResponse::responseSuccess($result);
+        ];
+        $res = Pay::wechat(config('wx.pay'))->miniapp($params);
+        return ApiResponse::responseSuccess($res);
     }
 
     /**
      * 通知微信支付结果
-     * @return \Symfony\Component\HttpFoundation\Response
-     * @throws \EasyWeChat\Kernel\Exceptions\Exception
+     * @return string
+     * @throws \Yansongda\Pay\Exceptions\InvalidSignException
      */
     public function notify(){
-        $app = WxService::getEasyApp();
-        $response = $app->handlePaidNotify(function ($message,$fail){
-            return true;
-        });
-        return $response;
+        $pay = Pay::wechat(config('wx.pay'));
+        $data = $pay->verify();
+        Log::debug('Wechat notify', $data->all());
+        return $pay->success();
     }
 
 
@@ -71,7 +68,6 @@ class WxPayController extends Controller{
      * @throws OperateFailedException
      * @throws ParamValidateFailedException
      * @throws \src\Exceptions\ResourceNotFoundException
-     * @throws \EasyWeChat\Kernel\Exceptions\InvalidConfigException
      */
     public function transfer(Request $request){
         $req = $request->all();
@@ -84,14 +80,14 @@ class WxPayController extends Controller{
             throw new OperateFailedException(ConstHelper::WRONG_ORDER_STATUS);
         }
         $params = [
-            'partner_trade_no' => $order->id, // 商户订单号，需保持唯一性(只能是字母或者数字，不能包含有符号)
-            'openid' => $order->receiver->openid,
-            'check_name' => 'NO_CHECK', // NO_CHECK：不校验真实姓名, FORCE_CHECK：强校验真实姓名
-            'amount' => ($order->price) * 100, // 企业付款金额，单位为分
-            'desc' => $order->title, // 企业付款操作说明信息。必填
+            'partner_trade_no' => time(),              //商户订单号
+            'openid' => $order->receiver->openid,        //收款人的openid
+            'check_name' => 'NO_CHECK',                //NO_CHECK：不校验真实姓名\FORCE_CHECK：强校验真实姓名
+            'amount' => ($order->price) * 100,         //企业付款金额，单位为分
+            'desc' => $order->title,                   //付款说明
+            'type' => 'miniapp'
         ];
-        $app = WxService::getEasyApp();
-        $result = $app->transfer->toBalance($params);
-        return ApiResponse::responseSuccess($result);
+        $res = Pay::wechat(config('wx.pay'))->transfer($params);
+        return ApiResponse::responseSuccess($res);
     }
 }

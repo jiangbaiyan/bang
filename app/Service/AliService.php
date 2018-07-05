682f0251d57c61e1575acc3e2e0457e3455d1821
Toby <987082332@qq.com>
2018-07-05 09:15:26 +0800

    支付宝配置

--- app/Service/AliService.php
+++ app/Service/AliService.php
@@ -14,11 +14,11 @@ use Yansongda\Pay\Pay;
 class AliService{
 
     private static $config = [
-            'app_id' => '',
+            'app_id' => '2017090608582098',
             'notify_url' => ConstHelper::HOST . 'pay/aliNotify',
             //'return_url' => 'http://yansongda.cn/return.php',
-            'ali_public_key' => '',
-            'private_key' => '',
+            'ali_public_key' => 'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA28Z6LQILxsm7xx9gxb5l7UvzwsSpIn7rzQwTKnnH8VNIObxPbEmzGL32vhfcrrWY+7qdaLlubGWwsCt3AoIC+ZfznDrsy0Sswsi9qXv/9ShQHhS0aUKem8eESyl3bpeeyZUP8L35vuH6Pqsvp/5aaA0wLQVKV4zv7hgi6p1D7PtB48o9F4dI4hUfDa/fFdRjVjaP5eUnoB0Vw0LnNj/8HAXtPTUD9TxkWXO2sLRhJQXhvqHrIR4tW/5zMkIL8/qKlovjpR8NRip5KD4tq9GWVa6oVlEbVCSvuNXKJJUhYT3+mfRZKTJbQFjDAYHxWh4k2kXAjSFDXNX4dSSmdIIzKQIDAQAB',
+            'private_key' => '7LEVJv7IIoX85VgZXjEqUg==4',
             'log' => [ // optional
                 'file' => '.storage/logs/alipay.log',
                 'level' => 'debug'
@@ -33,5 +33,4 @@ class AliService{
     public static function getAliPayApp(){
         return Pay::alipay(self::$config);
     }
-
 }
